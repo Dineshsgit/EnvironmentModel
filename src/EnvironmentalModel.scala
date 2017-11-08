8389ba43452437421488fcbc4441fe834fcc62d8
Dineshsgit <33451070+Dineshsgit@users.noreply.github.com>
2017-11-08 23:16:10 +1100

    Update EnvironmentalModel.scala

--- src/EnvironmentalModel.scala
+++ src/EnvironmentalModel.scala
@@ -100,7 +100,5 @@ object EnvironmentalModel extends App {
     print.close()
 
   }
-  calculateVariables("C:/Desktop/WeatherInputs/InputFile_WeatherStationsData.txt", "2016-08-21 02:05:37", "C:/Desktop/WeatherInputs/WeatherOutput.dat")
-
-
+  
 }
